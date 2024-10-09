bc3daf7538214efa8e6217a1129da04305c764f4
manitok <146006766+manitok@users.noreply.github.com>
2024-10-09 14:16:22 +0300

    task2

--- 3week/task2.py
+++ 3week/task2.py
@@ -1,1 +1,23 @@
-..
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+
+        ans = []
+
+        def backtracking(index: int, comb: list[int], total: int):
+            if total == target:
+                ans.append(comb[:])
+                return
+
+            for i in range(index, len(candidates)):
+                newTotal = total + candidates[i]
+
+                if newTotal > target:
+                    return
+
+                comb.append(candidates[i])
+                backtracking(i, comb, newTotal)
+                comb.pop()
+
+        backtracking(0, [], 0)
+        return ans
